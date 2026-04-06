e2cebe86728524934124247e501ca824922e823b
HemalshaAdasooriya <hemalsha2022uni@gmail.com>
2026-04-06 19:57:32 +0530

    add tailwindcss 🎨

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,9 @@ import ProductCard from './components/productCard'
 function App() {
 
   return (
-    <>
-      <Header/>
-      <ProductCard 
-          name= "laptop" 
-          image="https://picsum.photos/id/1/200/300" 
-          price="100,000"
-      />
-      <ProductCard 
-          name= "watch" 
-          image="https://picsum.photos/id/3/200/300" 
-          price="20,000"
-      />
-      <ProductCard 
-          name= "Phone" 
-          image="https://picsum.photos/id/4/200/300" 
-          price="50,000"
-      />
+    <div className='home border shadow'>
 
-    </>
+    </div>
   )
 }
 
