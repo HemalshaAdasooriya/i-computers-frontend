09208ce1bbff0a92051612dba1accd2cf78df570
HemalshaAdasooriya <hemalsha2022uni@gmail.com>
2026-04-11 01:19:33 +0530

    update 05

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import './App.css'
 import Header from './components/header'
 import ProductCard from './components/productCard'
-import Test from './components/test'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './Pages/homePage'
+import LoginPage from './Pages/loginPage'
+import RegisterPage from './Pages/registerPage'
+import AdminPage from './Pages/adminPage'
+
 
 function App() {
 
   return (
-    <div className='w-full h-[600px]'>
-      <Test/>
-    </div>
+    <BrowserRouter>
+            <div className="w-full h-screen bg-green-400">
+    
+                <Routes path="/">
+                    <Route path="/" element={<HomePage/>} />
+                    <Route path="/login" element={<LoginPage/>} />
+                    <Route path="/register" element={<RegisterPage/>} />
+                    <Route path="/admin" element={<AdminPage/>} />
+                    
+    
+                </Routes>
+    
+            </div>
+        </BrowserRouter>
   )
 }
 
