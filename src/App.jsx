dd4f3fef875a164b13dddf76fbdba28d02e96b4f
HemalshaAdasooriya <hemalsha2022uni@gmail.com>
2026-04-10 11:44:48 +0530

    update 02 💀

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,21 @@ import ProductCard from './components/productCard'
 function App() {
 
   return (
-    <div className='home border shadow'>
+    <div className='w-[600px] h-[600px] bg-gray-300 border'>
+      <div className='w-[500px] h-[500px] bg-yellow-300 flex flex-col items-center justify-center'>
+        <div className='w-[100px] h-[100px] bg-blue-400'>
 
+        </div>
+        <div className='w-[100px] h-[100px] bg-red-400'>
+
+        </div>
+        <div className='w-[100px] h-[100px] bg-green-400'>
+
+        </div>
+        <div className='w-[100px] h-[100px] bg-purple-400'>
+
+        </div>
+      </div>
     </div>
   )
 }
